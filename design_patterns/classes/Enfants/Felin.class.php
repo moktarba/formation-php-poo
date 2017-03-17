a39aeef891e64c3d18f4fdd1d393e2a2bb6bde1f
mouhamed <moktarba@hotmail.fr>
2017-03-17 14:48:41 +0100

    rename designs_ptterns to abstracts

--- design_patterns/classes/Enfants/Felin.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//Fatal error: Class Tigre may not inherit from final class (Felin) in
-// final  class Felin extends Animal{
-class Felin extends Animal{
-}
--- design_patterns/classes/Enfants/Fourmi.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Fourmi extends Insecte {
-  public function attaquer(){
-    echo "La fourmi attaque et mord <br>";
-  }
-}
- ?>
