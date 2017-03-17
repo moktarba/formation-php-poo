ce760b40e9b019bc9e66963e557470863dabc119
mouhamed <moktarba@hotmail.fr>
2017-03-17 13:08:54 +0100

    namespaces

---
+++ Namespace/Class1.php
@@ -0,0 +1,11 @@
+<?php
+namespace  Module1;
+
+  class Class1{
+
+    public function __construct(){
+      echo __Class__;
+    }
+  }
+
+ ?>
---
+++ Namespace/index.php
@@ -0,0 +1,12 @@
+<?php
+  require "Class1.php";
+
+  class Class1{
+
+    public function __construct(){
+      echo "Miam Miam";
+    }
+  }
+  new Class1;
+
+ ?>
